96d335a1687251a1805d9b86c61012e0f98c8b41
MiguelAngelSirpa <91285270+MiguelAngelSirpa@users.noreply.github.com>
2022-05-18 00:23:31 +0200

    Add files via upload

---
+++ p4.R
@@ -0,0 +1,4 @@
+mi_df<-data.frame(
+  "SIMBOLOS"=c(rep("trebol",13),rep("diamante",13),rep("corazon",13),rep("espada",13)),
+  "NUMEROS"=rep(c("1","2","3","4","5","6","7","8","9","10","J","Q","K"),2))
+ mi_df
